4a9da83aa684d57f27a2ded4d86c9a65d3fbcf51
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-21 20:08:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,6 @@ def all_supplies_in_holidays(holiday_hash)
       puts "#{holiday.to_s.split(" ").map{|w| w.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
   end
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
